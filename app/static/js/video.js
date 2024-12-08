545e5a233121a8ef580e7752233f5daa16fd8e39
ShresthShri <79257227+ShresthShri@users.noreply.github.com>
2024-12-08 23:23:47 +0000

    Test 3

--- app/static/js/video.js
+++ app/static/js/video.js
@@ -54,7 +54,7 @@ function saveVideo(button) {
 }
 
 /**
- * Function to remove a video from the saved video list and update the sidebar.
+ * Function to remove a video from the saved video list.
  * @param {HTMLElement} button - The button that triggered the remove action.
  */
 function removeVideo(button) {
