62bc86a9abd2445a41bf30cd2ca86d7fb5d3e98d
ShresthShri <79257227+ShresthShri@users.noreply.github.com>
2024-12-09 13:38:59 +0000

    Fixed T&C

--- app/routes/terms_routes.py
+++ app/routes/terms_routes.py
@@ -4,7 +4,7 @@ terms_bp = Blueprint('terms', __name__)
 
 
 @terms_bp.route('/', methods=['GET', 'POST'])
-#@login_required
-#Fixed it now
+
+
 def profile():
     return render_template('terms.html')
